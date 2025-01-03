aafe0a94129e85e834f93959dc388c6f4cc5cf7c
rease <cftcview@gmail.com>
2025-01-03 16:48:42 +0500

    rebuild

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,7 @@
 declare function pack(thing: any, proxyForFunctions?: TypeBuildProxyForFunctions | null, stringify?: boolean): string;
 declare function unpack(a: string, proxyForFunctions?: TypeParseProxyForFunctions): any;
-export declare type TypeBuildProxyForFunctions = (fn: Function) => string | null | undefined;
-export declare type TypeParseProxyForFunctions = (fname: string) => Function | null | undefined;
+export type TypeBuildProxyForFunctions = (fn: Function) => string | null | undefined;
+export type TypeParseProxyForFunctions = (fname: string) => Function | null | undefined;
 export { pack, unpack };
 declare const _default: {
     pack: typeof pack;
